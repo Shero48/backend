ce818773e92f5d3d701f4a8f70fce0e19c421856
Shero48 <sunils42212@gmail.com>
2025-03-29 21:36:36 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,9 +1,8 @@
 const mongoose=require('mongoose');
 const connectdata=async()=>{
-    let url=`mongodb+srv://sunil:sunil12@cluster0.3pwecso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    let url=`mongodb_url`
     await mongoose.connect(url)
     .then(res=>{
-        console.log(res);
         console.log("db is connected");
     })
     .catch(err=>{
